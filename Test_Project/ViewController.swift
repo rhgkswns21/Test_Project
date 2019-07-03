9b1740b55fb678ab24c7e40ba06190df93abe234
Hanjun Ko <rhkgsnws@naver.com>
2019-07-03 09:47:29 +0900

    Initial Commit

---
+++ Test_Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test_Project
+//
+//  Created by Hanjun Ko on 03/07/2019.
+//  Copyright © 2019 Hanjun Ko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
